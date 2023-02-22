08fb16d1df7473fe86121334d60a69b73c681e3b
saaasuuukaae <philipchef13@gmail.com>
2023-02-22 22:32:44 +0200

    .env API url fix error

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -2,7 +2,7 @@ import PocketBase from 'pocketbase';
 import {env} from "../env.mjs";
 
 
-const pb = new PocketBase(env.NEXT_PUBLIC_API_URL as string,);
+const pb = new PocketBase(env.NEXT_PUBLIC_API_URL);
 pb.autoCancellation(false)
 
 export default pb;
